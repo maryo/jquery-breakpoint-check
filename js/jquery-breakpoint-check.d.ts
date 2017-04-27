5b445e68c27b6c6639b3b91910867d50d4f7a51a
maryo <mario.dweller@seznam.cz>
2017-04-27 11:07:08 +0200

    Added typings

---
+++ js/jquery-breakpoint-check.d.ts
@@ -0,0 +1,8 @@
+interface JQueryStatic
+{
+    isBreakpoint(breakpoint: 'xs' | 'sm' | 'md' | 'lg'): boolean;
+    isXs(): boolean;
+    isSm(): boolean;
+    isMd(): boolean;
+    isLg(): boolean;
+}
